c5f3299e05d15fe04eb34f1eb1d516f1c1437a5f
Rog368401 <roryogorman36@gmail.com>
2024-03-04 16:47:43 +0000

    Some text changed on the home page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,12 +5,12 @@
         <div class="flex text-gray-100 pt-10">
             <div class="m-auto pt-4 pb-16 sm:m-auto w-4/5 block text-center">
                 <h1 class="sm:text-white text-5xl uppercase font-bold text-shadow-md pb-14">
-                    Do you want to become a developer?
+                    Do you want to become a God of War expert?
                 </h1>
                 <a 
                     href="/blog"
                     class="text-center bg-gray-50 text-gray-700 py-2 px-4 font-bold text-xl uppercase">
-                    Read More
+                    Start your Journey
                 </a>
             </div>
         </div>
@@ -23,47 +23,47 @@
 
         <div class="m-auto sm:m-auto text-left w-4/5 block">
             <h2 class="text-3xl font-extrabold text-gray-600">
-                Struggling to be a better web developer?
+                Struggling to get into God of War?
             </h2>
             
             <p class="py-8 text-gray-500 text-s">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus.
+                Well then you have come to the right place.
             </p>
 
             <p class="font-extrabold text-gray-600 text-s pb-9">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente magnam vero nostrum! Perferendis eos molestias porro vero. Vel alias.
+                Take some time and explore the farthest corners of this website and by the time your finished you will fully understand the Ghost of Sparta.
             </p>
 
             <a 
                 href="/blog"
                 class="uppercase bg-blue-500 text-gray-100 text-s font-extrabold py-3 px-8 rounded-3xl">
-                Find Out More
+                Knowledge Library
             </a>
         </div>
     </div>
 
     <div class="text-center p-15 bg-black text-white">
         <h2 class="text-2xl pb-5 text-l"> 
-            I'm an expert in...
+            Become an expert in...
         </h2>
 
         <span class="font-extrabold block text-4xl py-1">
-            Ux Design
+            Characters
         </span>
         <span class="font-extrabold block text-4xl py-1">
-            Project Management
+            Weapons
         </span>
         <span class="font-extrabold block text-4xl py-1">
-            Digital Strategy
+            Story
         </span>
         <span class="font-extrabold block text-4xl py-1">
-            Backend Development
+            Side-Quests
         </span>
     </div>
 
     <div class="text-center py-15">
         <span class="uppercase text-s text-gray-400">
-            Blog
+            Knowledge Library
         </span>
 
         <h2 class="text-4xl font-bold py-10">
@@ -89,12 +89,12 @@
                 <a 
                     href=""
                     class="uppercase bg-transparent border-2 border-gray-100 text-gray-100 text-xs font-extrabold py-3 px-5 rounded-3xl">
-                    Find Out More
+                    Knowledge Library
                 </a>
             </div>
         </div>
         <div>
-            <img src="https://i.ytimg.com/vi/zPUT7f9XFRU/maxresdefault.jpg" width="700" alt="">
+            <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/21a09fd4-9850-4d5c-b892-abbd4e007b8f/dfl5ifk-f17faa13-1b23-4e5a-be3c-3d497465780f.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzIxYTA5ZmQ0LTk4NTAtNGQ1Yy1iODkyLWFiYmQ0ZTAwN2I4ZlwvZGZsNWlmay1mMTdmYWExMy0xYjIzLTRlNWEtYmUzYy0zZDQ5NzQ2NTc4MGYuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0._d1wrERoc1spUH39qxMBX0bDGx1rPo_5Oj4SJRSErmI"  alt="">
         </div>
     </div>
 @endsection

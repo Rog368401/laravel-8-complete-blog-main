1ba69ec551fff6dc99074ad0c63c574fd95dac90
Rog368401 <roryogorman36@gmail.com>
2024-03-15 11:29:52 +0000

    style changes made to the enquiries page

--- resources/views/enquiries/index.blade.php
+++ resources/views/enquiries/index.blade.php
@@ -24,19 +24,19 @@
 
     @if (Auth::check())
         @foreach ($enquiries as $enquiry)
-            <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
+            <div class="sm:grid  w-4/5 mx-auto py-15 border-b border-gray-200">
                 @if ($enquiry->is_urgent)
-                    <h2 class="text-red-700 font-bold text-5xl pb-4">
+                    <h2 class="text-red-900 bg-yellow-500 font-bold text-5xl pb-4">
                         URGENT
                     </h2>
                 @endif
-                <h2 class="text-gray-700 font-bold text-5xl pb-4">
+                <h2 class="text-black font-bold text-5xl bg-red-900 pb-4">
                     {{ $enquiry->title }}
                 </h2>
-                <span class="text-gray-500">
+                <span class="text-black bg-red-900">
                     By <span class="font-bold italic text-gray-800">{{ $enquiry->email }}</span>
                 </span>
-                <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
+                <p class="text-xl text-black bg-red-900 pt-8 pb-10 leading-8 font-light">
                     {{ $enquiry->message }}
                 </p>
             </div>
@@ -46,7 +46,7 @@
     <div class="pt-15 w-4/5 m-auto">
         <a href="/enquiries/create"
             class="bg-blue-500 uppercase bg-transparent text-gray-100 text-xs font-extrabold py-3 px-5 rounded-3xl">
-            Make Enquiry
+            Ask the Fates A Question
         </a>
     </div>
 @endsection
